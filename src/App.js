ad756ffa14412dd88e93ea281287150d3e2816c2
isabelle-np <your_email@example.com>
2019-10-08 10:54:39 -0400

    Added basic structuring and styling to the main App. Also set the state to basic to-do tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [
+        {
+          id: 1,
+          name: "Laundry",
+          description: "Wash, dry, and fold dirty clothes",
+          completed: true
+        },
+        {
+          id: 2,
+          name: "Dishes",
+          description: "Load the dishwasher and put them all away",
+          completed: true
+        },
+        {
+          id: 3,
+          name: "Water Plants",
+          description: "Water the aloe, basil, and succulent plants",
+          completed: true
+        }
+      ]
+    }
+  }
+
+  render() {
+    const { tasks } = this.state
+    return (
+      <div className="App">
+        <h1>To Do</h1>
+        <ul>test</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
